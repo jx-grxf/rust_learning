a947c8d32ebb9738d11697dcd3ba6e46aba0d208
Johannes Grof <147989511+jx-grxf@users.noreply.github.com>
2026-09-04 15:17:07 +0200

    loops, if else and stdio

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,10 +1,39 @@
 // hello world
 
+// stdio 
+
+use std::io::{self, Write};
+
 fn main() {
+
+    // print
     println!("Hello world!");
 
+
+    // variables
     let number1 = 1;
     let number2 = 2;
 
+
+    // printing and adding numbers
     println!("{}", number1+number2);
+
+    // for loops
+
+    for _ in 1..5 {
+        println!("This is a for loop!\n");
+    }
+
+    // if else & scan
+
+    let age1 = 16;
+    let age2 = 18;
+
+    
+
+    if age1 > 17 && age2 > 17 {
+        println!("Both are allowed in!\n");
+    } else {
+        println!("Not allowed in!\n");
+    }
 }
